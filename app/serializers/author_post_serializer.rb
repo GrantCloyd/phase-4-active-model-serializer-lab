6c1d0a32fb4500d84fb483d515a1b28d753ca209
Grant Cloyd <dgcloyd@gmail.com>
2021-07-21 19:34:18 -0500

    commit tree 46ac7630f8bf5f187acb18826e1cffaa5d1c128a

---
+++ app/serializers/author_post_serializer.rb
@@ -0,0 +1,10 @@
+class AuthorPostSerializer < ActiveModel::Serializer
+  attributes :title, :short_content, :tags
+
+  def short_content
+  self.object.content.slice(0, 40) + '...'
+  end
+
+
+
+end
